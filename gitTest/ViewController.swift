8a1ab6ec535563797ed68b3076cad5f5b17580be
Ahmed Ahmedov <Murphy-mos@mail.ru>
2023-09-12 00:21:17 +0300

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Ahmed Ahmedov on 12.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("код")
+    }
+
+
+}
+
